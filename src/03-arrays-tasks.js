d17676965ce753f6c4003f96658d09df0d65b377
kotovnik <kotovnik2024tbilisi@gmail.com>
2023-10-30 03:16:48 +0400

    feat: start solved arrays task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -20,8 +20,8 @@
  *    ['Array', 'Number', 'string'], 'Date'    => -1
  *    [0, 1, 2, 3, 4, 5], 5    => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.indexOf(value);
 }
 
 /**
@@ -35,10 +35,11 @@ function findElement(/* arr, value */) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  let result = new Array(len).fill(0);
+  result = result.map((item, index) => 2 * index + 1);
+  return result;
 }
-
 
 /**
  * Returns the doubled array - elements of the specified array
